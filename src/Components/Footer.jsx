f7250eb379588910f719cb542a363a520b65fcc0
Brahmprakash Gautam <brahmprakash.gautam03@gmail.com>
2023-11-17 21:38:14 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -39,11 +39,11 @@ const Footer = () => {
 
       <div className="footer-2">
         <p>Terms & Condition | Privacy Policy </p>       
-        <p>
-          Copyright © 2023 <span>EXOCODE TECHNOLOGIES</span> | All rights
-          reserved        
-          Made By <span>Brahmprakash Gautam</span>            
-        </p>      
+        <div>
+          <p>Copyright © 2023 <span>EXOCODE TECHNOLOGIES</span> | All rights
+          reserved </p>       
+          <p>Made By <span>Brahmprakash Gautam</span></p>          
+        </div>      
         
       </div>
     </div>
